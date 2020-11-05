7a196b239b82d59585ba3919dab8b462f8f8a92e
Segara Bhagas Dagsapurwa <segara2410@gmail.com>
2020-11-06 06:38:00 +0700

    update player component display

--- src/Player.jsx
+++ src/Player.jsx
@@ -5,24 +5,30 @@ import {Draggable} from 'react-beautiful-dnd';
 const PlayerList = styled.div`
   width: 100%;
   background: ${props => props.isActive ? '#FFF' : '#000'};
+  margin-left: -8px;
+  border: 8px solid;
+  border-color: ${props => props.color};
   border-radius: 10px;
   margin-bottom: 15px;
   box-shadow: ${props => props.isDragging ? '0px 10px 20px rgba(0, 0, 0, 0.25)' : '0px 4px 4px rgba(0, 0, 0, 0.25)'};
   cursor: pointer;
 `
 
-const PlayerListText = styled.h6`
+const PlayerListText = styled.h1`
   color: #7B7B7B;
   text-align: center;
   font-family: sans-serif;
   font-size: 20px;
   margin: 0px;
-  padding: 15px;
+  padding-top: 3px;
+  padding-bottom: 25px;
   text-transform: none;
 `
 
 const PlayerImage = styled.img`
-  height: 50px;
+  position: relative;
+  top: 13px;
+  height: 40px;
 `
 
 function Player({player, index}) {
@@ -31,8 +37,8 @@ function Player({player, index}) {
   return (
     <Draggable draggableId={player.id} index={index}>
       {(provided, snapshot) => (
-        <PlayerList ref={provided.innerRef} onClick={() => setActive(active ? false : true)} isActive={active} isDragging={snapshot.isDragging} {...provided.draggableProps} {...provided.dragHandleProps}>
-          <PlayerListText>{player.content}</PlayerListText>
+        <PlayerList ref={provided.innerRef} color={player.color} onClick={() => setActive(active ? false : true)} isActive={active} isDragging={snapshot.isDragging} {...provided.draggableProps} {...provided.dragHandleProps}>
+          <PlayerListText><PlayerImage src={`${process.env.PUBLIC_URL}/characters/${player.content}.png`} /> {player.content}</PlayerListText>
         </PlayerList>
       )}
     </Draggable>
